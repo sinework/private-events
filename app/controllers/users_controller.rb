cf36245995934faf6263d5b5528f12de4cb1eb40
sinework <sine.s.amare@gmail.com>
2020-11-23 12:20:41 +0300

    Add: user as a resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+  @user= User.new
   end
 
   def create
@@ -10,4 +11,8 @@ class UsersController < ApplicationController
 
   def show
   end
+  private
+def user_params
+params.require(:user).permit(:username, :password)
+end
 end
