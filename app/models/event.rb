2e5816f6e2a2d15fda55f6e3ae1fbbab37584705
sinework <sine.s.amare@gmail.com>
2020-11-23 13:55:49 +0300

    Add: Add associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
-end
+    belongs_to :creator, class_name: "User", foreign_key: :user_id
+    has_many :attendees,  through: :attendances, class_name:"user", foreign_key: :user_id
+    has_many :attendances, dependent: :destroy
+ end
+    
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true, presence: true
+    validates :password, presence: true
     has_secure_password
+    has_many :events
+    has_many :attendances 
+    has_many :attended_events, class_name: 'Events', through: :attendances , foreign_key: :event_id
 end
