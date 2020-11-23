430786a0ed6c24897a6eed8794dc5c6a2625057f
sinework <sine.s.amare@gmail.com>
2020-11-23 12:36:42 +0300

    Add: Add sessions create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def destroy
-  end
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to root_path
+    else
+       redirect_to '/login'
+    end
+ end
+ 
+ def destroy
+  session.delete(:user_id)
+  # session[:user_id] = nil
+  redirect_to root_path, notice: "Logged out!"
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'signup' => 'users#new'
+get '/login' => 'sessions#new'
+post 'login' => 'sessions#create'
 resources :users
 end
