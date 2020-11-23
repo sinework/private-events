3b198be7223efd47b400fa950a396d00019aae93
sinework <sine.s.amare@gmail.com>
2020-11-23 16:30:33 +0300

    Update: update login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
        session[:user_id] = @user.id
        redirect_to '/'
     else
-       redirect_to '/login', notice: "didnt work"
+      # flash.now[:alert] = " didnt work"
+      #  redirect_to '/signup', notice: "didnt work"
+       format.html { redirect_to '/signup', notice: 'Tweet was successfully destroyed.' }
+      format.json { render json: @user.errors, status: :unprocessable_entity  }
+      
     end
  end
  
  def destroy
-  session.delete(:user_id)
-  # session[:user_id] = nil
+
+  session[:user_id] = nil
   redirect_to '/', notice: "Logged out!"
 end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def user_status
+        links = []
+        if logged_in?
+          links[0] = link_to 'New Event', new_event_path, class: 'nav-link'
+          links[1] = link_to "Log out", logout_path, method: "delete" , class: 'nav-link'
+          links[2] = current_user.username
+        else
+          links[0] = link_to "Login", 'login', class: 'nav-link'
+          links[1] = link_to 'Sign Up', 'signup', class: 'nav-link'
+        end
+        links
+    end
+    
+    #   def signed_in_only!
+    #     redirect_to new_user_session_path unless current_user
+    #   end
 end
