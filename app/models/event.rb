21b6958a94af5e84b8075ea637c9ae12d23c0664
sinework <sine.s.amare@gmail.com>
2020-11-26 15:04:28 +0300

    Update: update validation and associations in the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,13 @@
 class Event < ApplicationRecord
+    validates :title, :description, :date, presence: true
     belongs_to :creator, class_name: "User", foreign_key: :user_id
-    has_many :attendees,  through: :attendances, class_name:"user", foreign_key: :user_id
     has_many :attendances, dependent: :destroy
- end
+    has_many :attendees,  through: :attendances, class_name:"User", foreign_key: :user_id
+ 
+
+    scope :past, -> { where('date < ?', Date.today).order('created_at DESc') }
+    scope :upcoming, -> { where('date > ?', Date.today).order('created_at DESc') }
+  
+
+end
     
