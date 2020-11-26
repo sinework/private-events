134a7bd4d63994d3f2b3f7c501e2aeff9bcb220d
sinework <sine.s.amare@gmail.com>
2020-11-26 15:00:36 +0300

    Update: update attendance actions

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,13 @@
 class AttendancesController < ApplicationController
   def create
+    @attend = Attendance.new
+    @attend.user_id = current_user.id
+    @attend.event_id = params[:event_id]
+    redirect_to events_path, notice: 'Event added to attending list' if @attend.save
   end
 
-  def new
+  def destroy
+    @attend = Attendance.find_by(event_id: params[:id])
+    redirect_to events_path, notice: 'Event removed from attending list' if @attend.destroy
   end
 end
